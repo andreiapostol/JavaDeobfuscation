07fe4d13d5ac268d7b301dbbbd2175a0dbd378b5
Andrei-Laurentiu Apostol <andreiapostol@dhcp-10-248-206-229.eduroam.wireless.private.cam.ac.uk>
2020-02-13 03:10:38 +0000

    Progress with dependency retrieval

--- get_dependencies.py
+++ get_dependencies.py
@@ -12,7 +12,7 @@ import re
 import pygraphviz as pgv
 
 def is_terminal(node):
-    return node == None or (node.type in (FeatureNode.TOKEN, FeatureNode.IDENTIFIER_TOKEN))
+    return node == None or (node.type in (FeatureNode.TOKEN, FeatureNode.IDENTIFIER_TOKEN, FeatureNode.TYPE))
 
 def is_expression_node(node):
     return node.type == FeatureNode.FAKE_AST and node.contents == "EXPRESSION"
@@ -50,9 +50,9 @@ def get_if_arrays(root, id_mapping, source_mapping):
     return get_subtrees_based_on_function(root, id_mapping, source_mapping, \
         lambda node : node.type == FeatureNode.AST_ELEMENT and node.contents == "IF", set())
 
-def get_expression_arrays(root, id_mapping, source_mapping):
+def get_expression_operation_arrays(root, id_mapping, source_mapping):
     return get_subtrees_based_on_function(root, id_mapping, source_mapping, \
-        is_expression_node, set())
+        lambda node : is_expression_node(node) or is_operation_token(node), set())
 
 def get_terminal_variables(node, id_mapping, source_mapping):
     return get_subtrees_based_on_function(node, id_mapping, source_mapping, \
@@ -69,18 +69,20 @@ def get_member_select_dependencies(statement_type_node, id_mapping, source_mappi
 def handle_single_member_select(ms, id_mapping, source_mapping):
     branches = get_subtrees_based_on_function(ms, id_mapping, source_mapping, \
             lambda that_node : not is_terminal(that_node) and that_node != ms, set(), 2)
+    if (len(branches) < 2):
+        return []
     left_branch = branches[0]
     right_branch = branches[1]
     lb_variables = get_terminal_variables(left_branch, id_mapping, source_mapping)
     rb_variables = get_terminal_variables(right_branch, id_mapping, source_mapping)
-    if (len(lb_variables) == 1 and len(rb_variables) == 1):
+    if (len(rb_variables) == 1 and len(lb_variables) == 1):
         return [(lb_variables[0].contents, rb_variables[0].contents, ms.contents, 0)]
-    inner_right = handle_member_selects(right_branch, id_mapping, source_mapping)
-    if (len(lb_variables) == 0 or len(inner_right) == 0):
-        return inner_right
-    last = inner_right[-1]
-    inner_right.append((lb_variables[-1].contents, last[0], ms.contents, 0))
-    return inner_right
+    inner_left = handle_member_selects(left_branch, id_mapping, source_mapping)
+    if (len(rb_variables) == 0 or len(inner_left) == 0):
+        return inner_left
+    last = inner_left[-1]
+    inner_left.append((last[1], rb_variables[-1].contents, ms.contents, 0))
+    return inner_left
 
 def handle_member_selects(node, id_mapping, source_mapping):
     member_selects = get_member_select_dependencies(node, id_mapping, source_mapping)
@@ -89,27 +91,83 @@ def handle_member_selects(node, id_mapping, source_mapping):
         sol.extend(handle_single_member_select(ms, id_mapping, source_mapping))
     return sol
 
+# def filter_nodes(all_nodes, getter = lamcondition = lambda x : True):
+#      sol = []
+#      for node in all_nodes:
+#          if condition(node):
+#              sol.append(node)
+    
+
+def get_names(variables, getter = lambda x : x.contents, condition = lambda x : True):
+    sol = []
+    for variable in variables:
+        if condition(variable):
+            sol.append(getter(variable))
+    return sol
+
+def is_addition_or_substraction(op):
+    return op == "PLUS" or op == "MINUS" or op == "PLUS_ASSIGNMENT" or op == "MINUS_ASSIGNMENT"
+
+def is_multiplication_or_division(op):
+    return op == "MULTIPLY" or op == "DIVIDE" or op == "MULTIPLY_ASSIGNMENT" or op == "DIVIDE_ASSIGNMENT"
+
+def commutable_operations(inner, outer):
+    if (len(inner) == 0):
+        return True
+    if (outer == "ASSIGNMENT"):
+        return True
+    verify_function = None
+    if is_addition_or_substraction(outer):
+        verify_function = is_addition_or_substraction
+    elif is_multiplication_or_division(outer):
+        verify_function = is_multiplication_or_division
+    else:
+        return False
+    for stmt in inner:
+        if not verify_function(stmt):
+            return False
+    return True
+    
 def get_assignment_dependencies(statement_type_node, id_mapping, source_mapping, level = 0):
     branches = get_statement_branches(statement_type_node, id_mapping, source_mapping)
     sol = []
+    print(statement_type_node.contents, len(branches))
+    if (statement_type_node.contents == "IDENTIFIER"):
+        return sol
+    # print(statement_type_node.contents)
     if len(branches) == 2:
         left_variables = get_terminal_variables(branches[0], id_mapping, source_mapping)
         right_variables = get_terminal_variables(branches[1], id_mapping, source_mapping)
-        if (len(left_variables) > 1 and len(right_variables) > 1):
-            return sol
-        inner_expressions = get_expression_arrays(branches[1], id_mapping, source_mapping)
-        if len(right_variables) == 1 and len(inner_expressions) == 0:
+        lv = get_names(left_variables)
+        rv = get_names(right_variables)
+
+        print(lv, rv)
+
+        if len(right_variables) == 1:
             for lv in left_variables:
                     sol.append((lv.contents, right_variables[0].contents, statement_type_node.contents, level))
         else:
-            # if len(inner_expressions) == 0:
-            #     member_selects = get_member_select_dependencies(branches[1], id_mapping, source_mapping)
-            for expression in inner_expressions:
-                sol.extend(get_dependencies_from_expression(expression, id_mapping, source_mapping, level + 1))
-            
-            print(statement_type_node.contents, len(inner_expressions), inner_expressions)
+           
+            inner_dependencies = get_dependencies(branches[1], id_mapping, source_mapping, level + 1)
+            # for 
+            # sol.extend(inner_dependencies)
+            if (len(left_variables) == 1):
+                if (commutable_operations(list(set(map(lambda x : x[2], inner_dependencies))), statement_type_node) and len(inner_dependencies) > 0):
+                    for inner in inner_dependencies:
+                        sol.append((left_variables[0].contents, inner[0], statement_type_node.contents, level))
+                else:
+                    sol.append((left_variables[0].contents, '_', statement_type_node.contents, level))
+            sol.extend(inner_dependencies)
+    elif len(branches) == 1:
+        branch = branches[0]
+        variables = get_terminal_variables(branch, id_mapping, source_mapping)
+        if (len(variables) == 1):
+            sol.append((variables[0].contents, '1', statement_type_node.contents, level))
+    else:
+        print(branches)
     return sol
-
+# COMPUTED_FROM
+# LAST_WRITE
 def get_boolean_verifiers_array(node, id_mapping, source_mapping):
     return get_subtrees_based_on_function(node, id_mapping, source_mapping, \
         is_boolean_verifier_token, set())
@@ -123,8 +181,8 @@ def get_dependencies_from_expression(node, id_mapping, source_mapping, level = 0
         return dft
     statement_type_node = id_mapping[edges[0].destinationId]
     statement_type = statement_type_node.contents
-    if statement_type.find("ASSIGNMENT") or is_operation_token(statement_type_node):
-       return get_assignment_dependencies(statement_type_node, id_mapping, source_mapping, level)
+    # if statement_type.find("ASSIGNMENT") != -1 or is_operation_token(statement_type_node):
+    return get_assignment_dependencies(statement_type_node, id_mapping, source_mapping, level)
     return dft
 
 def get_dependencies_from_boolean(node, id_mapping, source_mapping, level = 0):
@@ -149,7 +207,7 @@ def get_dependencies_from_boolean(node, id_mapping, source_mapping, level = 0):
     
 
 def get_dependencies(root, id_mapping, source_mapping, level = 0):
-    top_expressions = get_expression_arrays(root, id_mapping, source_mapping)
+    top_expressions = get_expression_operation_arrays(root, id_mapping, source_mapping)
     top_booleans = get_boolean_verifiers_array(root, id_mapping, source_mapping)
     sol = []
     for top_expression in top_expressions:
@@ -166,7 +224,7 @@ def get_statement_subtrees(g):
     
     dependencies = get_dependencies(root, id_mapping, source_mapping)
     visual_graph = get_visual_graph(dependencies)
-    return dependencies, visual_graph
+    return list(set(dependencies)), visual_graph
     # print(source_mapping)
     # print(terminal_variables)
     # print(ifs)
@@ -184,7 +242,7 @@ def get_visual_graph(dependencies):
     for (start, end, edge_type, level) in dependencies:
         G.add_edge(start, end, color=get_color(edge_type))
         new_edge = G.get_edge(start, end)
-        new_edge.attr['label'] = edge_type
+        # new_edge.attr['label'] = edge_type
     return G
 
 if __name__ == "__main__":
@@ -192,8 +250,12 @@ if __name__ == "__main__":
     with open(filePath, "rb") as f:
         graph = Graph()
         graph.ParseFromString(f.read())
+        id_mapping = get_id_to_node_graph(graph)
+        source_mapping = get_source_dict_graph(graph)
+        # print(get_names(id_mapping.values(), lambda x : x, lambda x : x.contents == "MINUS"))
         dependencies, visual_graph = get_statement_subtrees(graph)
         print(dependencies)
+        
         visual_graph.layout() # default to neato
         visual_graph.draw('dependencies.png')
 
